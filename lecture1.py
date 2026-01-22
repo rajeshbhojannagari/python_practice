b9a9cab8cc72e708c60bd77b388930a7cbd26508
rajeshbhojannagari <rajeshbhojannagari@gmail.com>
2026-01-22 11:23:25 +0530

    started python practice

---
+++ lecture1.py
@@ -0,0 +1,77 @@
+# print("Hello")
+# print(2)
+# name=input("Enter a Name: ")
+# age=int(input("Enter your age: "))
+# gpa=float(input("Enter Your cgpa: "))
+# IsBool=True
+# print(f"Hello {name}")
+# print(f"Your age is {age}")
+# print(f"Your CGPA is {gpa}")
+# print(IsBool)
+# side=float(input("Enter the side length: "))
+# area_of_square=pow(side,2)
+# print(area_of_square)
+# num1=int(input("Enter a number1: "))
+# num2=float(input("Enter a number2: "))
+# sum=num1+int(num2)
+# print(sum)
+# print(type(sum))
+# mark1=float(input("Enter the marks of sub1: "))
+# mark2=float(input("Enter the marks of sub2: "))
+# mark3=float(input("Enter the marks of sub3: "))
+# mark4=float(input("Enter the marks of sub4: "))
+# mark5=float(input("Enter the marks of sub5: "))
+# avg=(mark1+mark2+mark3+mark4+mark5)/5
+# print(f"your avg marks are {avg:.2f}")
+
+# while True:
+#     ItemName=input("Enter Item Name: ")
+#     Quantity=int(input("Enter Number of Items: "))
+#     Price=float(input("Enter the price: "))
+#     cost=Quantity*Price
+#     final=cost+0.18*cost
+#     print(f"Cost is {cost}")
+#     print(f"After gst {final}")
+
+#     inputKey=input("Enter y to terminate code: ")
+#     if inputKey.lower()=='y':
+#         print("Thank You")
+#         break
+
+# num1=int(input("Enter a number1: "))
+# num2=int(input("Enter a number2: "))
+# sum=num1+num2
+# sub=num1-num2
+# div=num1/num2
+# flodiv=num1//num2
+# mul=num1*num2
+# exp=num1**num2
+# print(sum)
+# print(sub)
+# print(div)
+# print(flodiv)
+# print(mul)
+# print(exp)
+# sum=0
+# while True:
+#     expences=int(input("Enter your expences: "))
+#     sum=sum+expences
+#     choice=input("Enter y to terminate: ")
+#     if choice.lower()=='y':
+#         print("Thank you")
+#         break
+# print(sum)
+# number1=int(input("Enter a number1: "))
+# number2=int(input("Enter a number2: "))
+# number3=int(input("Enter a number3: "))
+# if (number1>number2 and number1>number3):
+#     print(f"Largest Number is {number1}")
+# elif (number2>number1 and number2 >number3):
+#     print(f"The Largest Number is {number2}")
+# else:
+#     print(f"The Largest Number is {number3}")
+# principle=int(input("Enter the amount: "))
+# time=float(input("Enter the time: "))
+# rate=float(input("Enter the rate: "))
+# si=principle+(principle*time*rate)/100
+# print(si)
---
+++ lecture2.py
@@ -0,0 +1,26 @@
+# password="vikram"
+# while True:
+#     enter=input("Guess a password: ")
+#     if(enter==password):
+#         print("You have entered password right")
+#         break
+#     else:
+#         print("Try Again")
+
+# amount=int(input("Enter the amount you want to withdraw  500 and 200 notes are available: "))
+# if amount>=200 and amount%100==0 and amount!=300:
+#     print("Discharging cash")
+# else:
+#     print("Denomination not available")
+
+time1=int(input("Enter a hour in 24 hrs format: "))
+while True:
+    if (time1>=0 and time1<12):
+        print("AM")
+        break
+    elif(time1>=12 and time1<24):
+        print("PM")
+        break
+    else:
+        time1=int(input("Enter a hour in 24 hrs format: "))  
+        

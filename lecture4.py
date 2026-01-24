daa74a2bf81ca1989ede89eb40a1dd24b9dad920
rajeshbhojannagari <rajeshbhojannagari@gmail.com>
2026-01-24 09:23:09 +0530

    Learning about dictionoary

---
+++ lecture4.py
@@ -0,0 +1,15 @@
+# student_grades={
+#     "Alice":"A",
+#     "Age":20,
+#     "Grade":"A",
+#     "Subject":"CSE"
+# }
+# print(student_grades)
+Color_Fruit={
+    "Mango":"Yellow",
+    "Grapes":"Green",
+    "Apple":"Red",
+    "Banana":"Yellow"
+}
+print(Color_Fruit.keys())
+print(Color_Fruit.values())

821ede2c4daf87a6e6e1e5348416dca37938b6b8
rajeshbhojannagari <rajeshbhojannagari@gmail.com>
2026-01-24 10:21:44 +0530

    added more code about dict

--- lecture4.py
+++ lecture4.py
@@ -25,9 +25,24 @@
 # else:
 #     print("not found")
 
-Details={'name':'Rajesh','age':20,'City':'Hyderabad'}
-print(Details)
-del Details['age']
-print(Details)
-print(len(Details))
-print('name' in Details)
+# Details={'name':'Rajesh','age':20,'City':'Hyderabad'}
+# print(Details)
+# del Details['age']
+# print(Details)
+# print(len(Details))
+# print('name' in Details)
+# for i in Details:
+#     print(i,Details[i])
+# print(len(Details))
+
+data={
+    'id':101,
+    'info':{
+        'name':'Alice',
+        'age':20
+    }
+}
+# for i in data:
+print(data['info']['name'])
+print(len(data))
+print(data.items())

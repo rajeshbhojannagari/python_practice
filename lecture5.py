ae58b2e7e3a55f164fa1335c6169bb23860d941c
rajeshbhojannagari <rajeshbhojannagari@gmail.com>
2026-01-27 14:32:10 +0530

    added code to lecture5.py file

--- lecture5.py
+++ lecture5.py
@@ -1,6 +1,27 @@
-class Student:
-    name="Rajesh"
-    grade="A"
-student1=Student()
-print(student1.name)
-print(student1.grade)
+# class Student:
+#     name="Rajesh"
+#     grade="A"
+# student1=Student()
+# print(student1.name)
+# print(student1.grade)
+
+# class Student:
+#     def __init__(self,brand,model,year):
+#         self.brand=brand
+#         self.model=model
+#         self.year=year
+# Student1=Student("TATA","High",2025)
+# print(Student1.brand)
+# print(Student1.model)
+# print(Student1.year)
+
+# class Bank:
+#     def __init__(self,bankAcc,branch,withdraw):
+#         self.bankAcc=bankAcc
+#         self.branch=branch
+#         self.withdraw=withdraw
+#     def bankDet(self):
+#         print(f"You have Account in {self.bankAcc} and branch is {self.branch} and the money withdraw is {self.withdraw}")
+# state=Bank("ICICI","Hyderabad",500)
+# state.bankDet()
+

8b3fe7a9cdd7b6abfb25a4e995314321aecf6402
rajeshbhojannagari <rajeshbhojannagari@gmail.com>
2026-01-28 12:13:38 +0530

    studing about args and kwarg

---
+++ Lecture6.py
@@ -0,0 +1,17 @@
+# def num(*args):
+#     total=0
+#     for i in args:
+#         total+=i
+#     return total
+# print(num(1,23,8,45,8))
+
+# def names(**kwargs):
+#     for i,j in kwargs.items():
+#         print(i,j)
+# (names(name="Rajesh",age=20,college="LPU"))
+
+# def values(msg,*args,**kwargs):
+#     print(msg)
+#     print(args)
+#     print(kwargs)
+# values(("Hey Buddy"),1,2,33,4,name="Rajesh",age=20,city="Hyderabad")

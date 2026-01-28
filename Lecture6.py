66550230ac648947d7436e35832b8747ef8eb2c5
rajeshbhojannagari <rajeshbhojannagari@gmail.com>
2026-01-28 12:30:24 +0530

    more code about lambda function

--- Lecture6.py
+++ Lecture6.py
@@ -15,3 +15,21 @@
 #     print(args)
 #     print(kwargs)
 # values(("Hey Buddy"),1,2,33,4,name="Rajesh",age=20,city="Hyderabad")
+
+# def sq(x):
+#     return x**x
+# print(sq(2))
+
+# y=lambda x:x**2
+# print(y(5))
+
+
+# y=lambda a,b:(a+b)
+# print(y(5,3))
+
+# a=int(input("a: "))
+# b=int(input("b: "))
+# y=lambda a,b: a if(a>b) else  b
+# print(y(a,b))
+
+

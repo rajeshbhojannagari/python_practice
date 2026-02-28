2a0f8048240352beb63b3d47afff41463376732c
rajeshbhojannagari <rajeshbhojannagari@gmail.com>
2026-02-28 15:13:04 +0530

    changes

--- practice1.py
+++ practice1.py
@@ -1,7 +1,15 @@
-print("Hello World")
-name="Rajesh"
-age=20
-course="BTech"
-print(name)
-print(age)
-print(3*course)
+# print("Hello World")
+# name="Rajesh"
+# age=20
+# course="BTech"
+# age1=age
+# print(name)
+# print(age)
+# print(3*course)
+# print(bool(0))
+# age=25
+# print(age)
+# print(age1)
+
+a=None
+print(a)

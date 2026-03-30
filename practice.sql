a8911b12bb3bf10ea3357409a56fbe8fbbfe11a9
rajeshbhojannagari <rajeshbhojannagari@gmail.com>
2026-03-30 13:09:42 +0530

    code

--- practice.sql
+++ practice.sql
@@ -1,2 +1,3 @@
--- code 1
+-- code 1: Find the difference between the total number of CITY entries in the table and the number of distinct CITY entries in the table.
+--The STATION table is described as follows:
 --SELECT count(CITY)- count(distinct CITY) FROM STATION;

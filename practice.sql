cfa3a9593efe0b274ab2e9a576a05410f741fead
rajeshbhojannagari <rajeshbhojannagari@gmail.com>
2026-03-31 23:21:57 +0530

    changes

--- practice.sql
+++ practice.sql
@@ -2,12 +2,6 @@
 --The STATION table is described as follows:
 --SELECT count(CITY)- count(distinct CITY) FROM STATION;
 
-
-
-
-
-
-
 --code 2: Query the two cities in STATION with the shortest and longest CITY names, as well as their respective lengths (i.e.: number of characters in the name). If there is more than one smallest or largest city, choose the one that comes first when ordered alphabetically.
 --The STATION table is described as follows:
 -- (SELECT CITY,LENGTH(CITY) as city_length FROM STATION ORDER BY

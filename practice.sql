a4c8b9f6c401bcd206a1957c80e593372b78382e
rajeshbhojannagari <rajeshbhojannagari@gmail.com>
2026-04-01 11:03:00 +0530

    added another code

--- practice.sql
+++ practice.sql
@@ -27,3 +27,6 @@
 
 --code 9: Query the list of CITY names from STATION that do not start with vowels and do not end with vowels. Your result cannot contain duplicates.
 --SELECT DISTINCT CITY FROM STATION WHERE CITY not REGEXP '^[AEIOU].' AND CITY not REGEXP '[AEIOU]$';
+
+--code 10: Query the Name of any student in STUDENTS who scored higher than  Marks. Order your output by the last three characters of each name. If two or more students both have names ending in the same last three characters (i.e.: Bobby, Robby, etc.), secondary sort them by ascending ID.
+--SELECT Name FROM STUDENTS WHERE Marks>75 ORDER BY Right(Name,3) ,ID ASC; 

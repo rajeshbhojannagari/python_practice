72e5af5c2009af644b9c9f99aae50b66dc173ff2
rajeshbhojannagari <rajeshbhojannagari@gmail.com>
2026-04-01 11:05:05 +0530

    added one more code

--- practice.sql
+++ practice.sql
@@ -30,3 +30,6 @@
 
 --code 10: Query the Name of any student in STUDENTS who scored higher than  Marks. Order your output by the last three characters of each name. If two or more students both have names ending in the same last three characters (i.e.: Bobby, Robby, etc.), secondary sort them by ascending ID.
 --SELECT Name FROM STUDENTS WHERE Marks>75 ORDER BY Right(Name,3) ,ID ASC; 
+
+--code 11: Write a query that prints a list of employee names (i.e.: the name attribute) from the Employee table in alphabetical order.
+--SELECT name FROM Employee order by name asc;

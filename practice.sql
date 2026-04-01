766f73e0bacbd206ce8e1514379349d295e52fce
rajeshbhojannagari <rajeshbhojannagari@gmail.com>
2026-04-01 11:08:06 +0530

    one more question added

--- practice.sql
+++ practice.sql
@@ -33,3 +33,6 @@
 
 --code 11: Write a query that prints a list of employee names (i.e.: the name attribute) from the Employee table in alphabetical order.
 --SELECT name FROM Employee order by name asc;
+
+--code 12: Write a query that prints a list of employee names (i.e.: the name attribute) for employees in Employee having a salary greater than $2000 per month who have been employees for less than 10 months. Sort your result by ascending employee_id.
+--SELECT name from Employee WHERE salary>2000 AND months<10 ORDER BY employee_id ASC;
